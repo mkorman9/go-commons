5c86ce381d85caa9cbbe7bfdb401c00696cb571d
Michał Korman <michal.korman@spokko.com>
2022-06-09 13:59:14 +0200

    add convertion beteen epoch time and Time pointers

--- utils/time.go
+++ utils/time.go
@@ -5,3 +5,21 @@ import "time"
 func TimePtr(t time.Time) *time.Time {
 	return &t
 }
+
+func TimePtrToUnix(t *time.Time) *int64 {
+	if t == nil {
+		return nil
+	}
+
+	value := (*t).Unix()
+	return &value
+}
+
+func UnixPtrToTime(u *int64) *time.Time {
+	if u == nil {
+		return nil
+	}
+
+	value := time.Unix(*u, 0)
+	return &value
+}
